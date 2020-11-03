d217bfd6566d8d4f439aeff9ce003d6516b6da1f
Yunjia Wang <yunjiw1@uci.edu>
2020-11-03 00:23:38 -0800

    order analysis in progress

--- src/main/java/com/ucidw/service/impl/TradeServiceImpl.java
+++ src/main/java/com/ucidw/service/impl/TradeServiceImpl.java
@@ -6,12 +6,15 @@ import com.ucidw.mapper.TradeMapper;
 import com.ucidw.service.IAdService;
 import com.ucidw.service.ITradeService;
 import com.ucidw.vo.AdVo;
+import com.ucidw.vo.MemberVo;
 import com.ucidw.vo.TradeOrderVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Service("iTradeService")
@@ -22,6 +25,20 @@ public class TradeServiceImpl implements ITradeService {
 
     @Override
     public ServerResponse<List<TradeOrderVo>> countTradeData() {
+        String date = "";
+        Map<String,Object> map = new HashMap<String, Object>();
+        List<String> cityNames = new ArrayList<String>();
+        List<String> growDataList = new ArrayList<String>();
+        List<TradeOrderVo> list = mapper.tradeData();
+        for (TradeOrderVo me : list) {
+            if (me.getAreatype() != "City") continue;
+            date = me.getDt();
+            cityNames.add(me.getCityname());
+            growDataList.add(me.getTotalcount());
+        }
+        map.put("date", date);
+        map.put("cityNames",cityNames);
+        map.put("seriesData",growDataList);
         return ServerResponse.createBySuccess(mapper.tradeData());
     }
 }
